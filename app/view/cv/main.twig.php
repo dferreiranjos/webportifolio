799f66896f484b46db240ee5cfe0f25807a75008
dferreiranjos <dferreiranjos@yahoo.com.br>
2022-01-04 22:40:34 -0300

    update left side cv

---
+++ app/view/cv/main.twig.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{BASE}}css/stylecv.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" integrity="sha512-5A8nwdMOWrSz20fDsjczgUidUBR8liPYU+WymTZP1lmY9G6Oc7HlZv156XqnsgNUzTyMefFTcsFH/tnJE/+xBg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Currículo</title>
+</head>
+<body>
+    <div class="container">
+        <div class="left_side">
+            <div class="profileText">
+                <div class="imgBx">
+                    <img class="myphoto" src="{{BASE}}img/myphoto.png" alt="user photo">
+                </div>
+                <h1>Daniel Anjos<br><span>Web Developer</span></h1>
+            </div>
+
+            <div class="contactInfo">
+                <h3 class="title">Contato</h3>
+                <ul>
+                    <li>
+                        <span class="icon"><i class="fa fa-phone" aria-hidden="true"></i></span>
+                        <span class="text">31 98717 7764</span>
+                    </li>
+                    <li>
+                        <span class="icon"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+                        <span class="text">dferreiranjos@yahoo.com.br</span>
+                    </li>
+                    <li>
+                        <span class="icon"><i class="fa fa-globe" aria-hidden="true"></i></span>
+                        <span class="text">danielanjosfullstack.com.br</span>
+                    </li>
+                    <li>
+                        <span class="icon"><i class="fa fa-linkedin" aria-hidden="true"></i></span>
+                        <span class="text">www.linkedin.com/VER</span>
+                    </li>
+                    <li>
+                        <span class="icon"><i class="fa fa-map-marker" aria-hidden="true"></i></span>
+                        <span class="text">BH - MG - Brasil</span>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="contactInfo education">
+                <h3 class="title">Formação</h3>
+                <ul>
+                    <li>
+                        <h5>2021 - Em andamento</h5>
+                        <h4>Formação PHP FullStack</h4>
+                        <h4>UpInsider</h4>
+                    </li>
+                    <li>
+                        <h5>2021 - Em andamento</h5>
+                        <h4>Formação PHP FullStack</h4>
+                        <h4>UpInsider</h4>
+                    </li>
+                    <li>
+                        <h5>2021 - Em andamento</h5>
+                        <h4>Formação PHP FullStack</h4>
+                        <h4>UpInsider</h4>
+                    </li>
+                    
+                </ul>
+            </div>
+
+        </div>
+
+
+        <div class="right_side"></div>
+    </div>
+</body>
+</html>
